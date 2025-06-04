f4c50e2338dbef10cf0f19c3964544a0adc28e11
Mohamed Mousa <77318165+mohamedmousa77@users.noreply.github.com>
2025-06-05 00:58:50 +0200

    handling icons images

--- multi-step-form/src/app/components/step5-confirming-registration/step5-confirming-registration.component.ts
+++ multi-step-form/src/app/components/step5-confirming-registration/step5-confirming-registration.component.ts
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
 export class Step5ConfirmingRegistrationComponent {
   title = 'Thank you!';
   desc = 'Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.';
-  thankYouImage = 'assets/images/icon-thank-you.svg';
+  // thankYouImage = 'assets/images/icon-thank-you.svg';
 }
